1d49c2905652ae21807e724e0e6b1d1d31cb2609
Sabrina Li <sabrinali0312@gmail.com>
2019-05-05 12:04:56 -0400

    submit form and listen to respons

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,15 +5,16 @@ router = express.Router();
 router.get("/friends",(req,res)=>{    
     res.send(friendsData);
 });
+
 router.post("/friends",(req,res,next)=>{
-    console.log( req.body.group1);
     const newFriend = {
             name: req.body.name,
             photo: req.body.photo,
             scores: [req.body.group1,req.body.group2,req.body.group3,req.body.group4,req.body.group5,req.body.group6]
         }
     friendsData.push(newFriend);
-    res.send("OK");
+    console.log(newFriend);
+    res.send(newFriend);
 })
 
 
