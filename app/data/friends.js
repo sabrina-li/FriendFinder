3ee8fe91a724036f307ca349e5ac7ec455cb6585
Sabrina Li <sabrinali0312@gmail.com>
2019-05-05 15:28:04 -0400

    add photo for me

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsData =
 [
     {
         "name": "Sabrina",
-        "photo": "https://cdn.eso.org/images/screen/eso1907a.jpg",
+        "photo": "https://scontent.fatl1-1.fna.fbcdn.net/v/t31.0-8/11265564_850233181697924_7178732803248699873_o.jpg?_nc_cat=100&_nc_ht=scontent.fatl1-1.fna&oh=20d70217616ad61946224ba63308aa39&oe=5D5EDACB",
         "fb":"https://www.facebook.com/sabrina.li.9828",
         "scores": ["4","3","4","5","2","5","3","3","3","3"]
     }
