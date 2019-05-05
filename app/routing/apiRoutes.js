d462df5d28481de8d73dca5e8e2fc49ec0927827
Sabrina Li <sabrinali0312@gmail.com>
2019-05-05 13:13:13 -0400

    add modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,11 +12,38 @@ router.post("/friends",(req,res,next)=>{
             photo: req.body.photo,
             scores: [req.body.group1,req.body.group2,req.body.group3,req.body.group4,req.body.group5,req.body.group6]
         }
+    
+    const match = matchNewFriend(newFriend);
     friendsData.push(newFriend);
-    console.log(newFriend);
-    res.send(newFriend);
+    res.send(match);
 })
 
+
+const matchNewFriend = (newFriend)=>{
+    const newScores = newFriend.scores;
+    let match = friendsData[0],//ensure match is found
+        minTotalDiff=41;//max diff is 40 
+    friendsData.forEach(friend=>{
+        try{
+            thisDiff = diff(newScores,friend.scores);
+            if(thisDiff<minTotalDiff){
+                minTotalDiff = thisDiff;
+                match = friend
+            }
+        }catch(err){
+            console.error(err);
+        }
+    })
+    return match;
+}
+const diff = (score1,score2)=>{
+    let totalDiff=0;
+    if (score1.length !== score2.length) throw "length doesn't match!"
+    for (var i=0;i<score1.length;i++){
+        totalDiff += Math.abs(score1[i]-score2[i]);
+    }
+    return totalDiff;
+}
 
 
 
